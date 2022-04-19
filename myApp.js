dacd558565ec9e539cd4f4c1955660a18314c014
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-04-19 00:58:52 +0000

    Returns a html file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello Express')
+    console.log('Dir Name: ', path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 
 module.exports = app;
