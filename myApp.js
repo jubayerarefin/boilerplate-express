1e26a102eea373065aa025fdf96b9f5139df9955
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-04-21 15:30:22 +0000

    Adds a middleware to change res/req and log

--- myApp.js
+++ myApp.js
@@ -2,14 +2,20 @@ var express = require('express');
 var path = require('path');
 var app = express();
 require('dotenv').config()
-// const mySecret = process.env['MESSAGE_STYLE'];
+const MESSAGE_STYLE = process.env['MESSAGE_STYLE'];
+
+app.use(function(req, res, next) {
+  console.log(req.method,req.path,'-',req.ip);
+  next();
+})
+
 app.get('/',(req,res) => {
-    console.log('Dir Name: ', path.join(__dirname + '/public'));
+    // console.log('Dir Name: ', path.join(__dirname + '/public'));
     res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 
 app.get('/json',(req,res) => {
-    var data = process.env.MESSAGE_STYLE == 'uppercase' ? {"message": "HELLO JSON"}:{"message": "Hello json"};
+    var data = MESSAGE_STYLE == 'uppercase' ? {"message": "HELLO JSON"} : {"message": "Hello json"};
     res.json(data);
 })
 
