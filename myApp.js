a3cc3e59e845500d59e3674459bb2d74b66fa2eb
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-04-21 00:45:21 +0000

    Adds static file path & middleware

--- myApp.js
+++ myApp.js
@@ -3,8 +3,11 @@ var path = require('path');
 var app = express();
 
 app.get('/',(req,res) => {
-    console.log('Dir Name: ', path.join(__dirname, '/views/index.html'));
+    console.log('Dir Name: ', path.join(__dirname + '/public'));
     res.sendFile(path.join(__dirname, '/views/index.html'));
 })
+
+app.use('/public',express.static('public'))
+express.static(path.join(__dirname + '/public'));
 
 module.exports = app;
