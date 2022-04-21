0fdeb1cbb3481a6b72b92b67fc045994ab46e05f
Sharif Jubayer Arefin <contact@jubayerarefin.info>
2022-04-21 01:09:45 +0000

    Adds /json and returns a json response in body

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+require('dotenv').config()
+// const mySecret = process.env['MESSAGE_STYLE'];
 app.get('/',(req,res) => {
     console.log('Dir Name: ', path.join(__dirname + '/public'));
     res.sendFile(path.join(__dirname, '/views/index.html'));
+})
+
+app.get('/json',(req,res) => {
+    var data = process.env.MESSAGE_STYLE == 'uppercase' ? {"message": "HELLO JSON"}:{"message": "Hello json"};
+    res.json(data);
 })
 
 app.use('/public',express.static('public'))
